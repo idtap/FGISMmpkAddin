bc4860c5973ce5214af38293d312553c0f1d7321
winhu <winhu@idtech.com.tw>
2023-11-27 09:20:33 +0800

    update

--- AutoMap2MmpkButton.cs
+++ AutoMap2MmpkButton.cs
@@ -14,70 +14,36 @@ using ArcGIS.Desktop.Catalog;
 
 namespace FGISMmpkAddin
 {
-        internal class AutoMap2MmpkButton : Button
+    internal class AutoMap2MmpkButton : Button
+    {
+        private GenMap2MmpkWindow genMap2MmpkWindow = null;
+
+        protected override void OnClick()
         {
-                protected override async void OnClick()
-                {
-                        try
-                        {
-                                IGPResult toolResult = await Execute_ApplySymbologyFromFeatureLayer();
-                                Geoprocessing.ShowMessageBox(toolResult.Messages, "GP Messages", 
-                                        toolResult.IsFailed ? GPMessageBoxStyle.Error : GPMessageBoxStyle.Default);
-                        }
-                        catch (Exception ex)
-                        {
-                                MessageBox.Show($@"Error: {ex}");
-                        }
-                }
-
-                public async Task<IGPResult> Execute_ApplySymbologyFromFeatureLayer()
-                {
-                        var environments = Geoprocessing.MakeEnvironmentArray(overwriteoutput: true);
-
-                        var prj = Project.Current;
-                        var map = MapView.Active;
-
-                        var featLayers = map.Map.Layers.OfType<FeatureLayer>().Where(l => l.Name.StartsWith ("Crimes", StringComparison.OrdinalIgnoreCase));
-                        if (featLayers.Count() < 1)
-                        {
-                                MessageBox.Show("Unable to find Crimes layer: the topmost Crime layer is the target layer for the symbology, the symbology source is a lyrx file");
-                                return null;
-                        }
-
-                        // the first layer in TOC is without any symbology
-                        FeatureLayer targetLayer = featLayers.ElementAt(0);
-
-                        // the second layer has the symbology
-                        // symbology from the 2nd layer will be applied to the first layer
-                        //Create instance of BrowseProjectFilter using the id for Pro's Lyrx files filter
-                        BrowseProjectFilter bf = new BrowseProjectFilter("esri_browseDialogFilters_layers_lyrx");
-                        //Display the filter in an Open Item dialog
-                        OpenItemDialog lyrxOpenDlg = new OpenItemDialog
-                        {
-                                Title = "Symbology Input Layer",
-                                MultiSelect = false,
-                                BrowseFilter = bf
-                        };
-                        bool? ok = lyrxOpenDlg.ShowDialog();
-                        if (!ok.HasValue || !ok.Value) return null;
-                        // Full path for the lyrx file
-                        var lryxItem = lyrxOpenDlg.Items.FirstOrDefault().Path;
-
-                        var sourceField = "Major_Offense_Type";
-                        var targetField = "Major_Offense_Type";
-                        var fieldType = "VALUE_FIELD";
-                        String fieldInfo = String.Format("{0} {1} {2}", fieldType, sourceField, targetField); // VALUE_FIELD NAME NAME")
-                        MessageBox.Show("Field Info for symbology: " + fieldInfo);
-
-                        var parameters = Geoprocessing.MakeValueArray(targetLayer, lryxItem, fieldInfo); //, symbologyFields, updateSymbology);
-
-                        // Does not Add outputs to Map as GPExecuteToolFlags.AddOutputsToMap is not used
-                        GPExecuteToolFlags executeFlags = GPExecuteToolFlags.RefreshProjectItems | GPExecuteToolFlags.GPThread | GPExecuteToolFlags.AddToHistory;
-
-                        IGPResult result = await Geoprocessing.ExecuteToolAsync("ApplySymbologyFromLayer_management", parameters, null, null, null, executeFlags);
-                        return result;
-                }
-
-
+            if (genMap2MmpkWindow != null)
+            {
+                genMap2MmpkWindow.Show();
+                return;
+            }
+            GenMap2MmpkWindow_Create();
+            genMap2MmpkWindow.Show();
         }
+
+        public void GenMap2MmpkWindow_Create()
+        {
+            genMap2MmpkWindow = new GenMap2MmpkWindow();
+            genMap2MmpkWindow.Owner = FrameworkApplication.Current.MainWindow;
+            genMap2MmpkWindow.Closing += GenMap2MmpkWindow_Closing;
+            genMap2MmpkWindow.Closed += (o, e) => { 
+                //genMap2MmpkWindow = null; 
+            };
+        }
+
+        void GenMap2MmpkWindow_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            e.Cancel = true;
+            genMap2MmpkWindow.Hide();
+        }
+
+    }
 }
